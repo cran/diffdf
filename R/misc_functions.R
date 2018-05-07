b4482704fceeaa390884d096e951044ffde488b5
Craig Gower <craig.gower@roche.com>
2018-05-07 10:17:02 +0000

    version 1.0.0

---
+++ R/misc_functions.R
@@ -0,0 +1,48 @@
+
+
+
+#' factor_to_character
+#' 
+#' Takes a dataframe and converts any factor variables to character
+#' @param dsin input dataframe 
+#' @param vars variables to consider for conversion. Default NULL will consider 
+#' every variable within the dataset
+factor_to_character <- function( dsin , vars = NULL){
+    
+    if ( is.null(vars) ) vars = names(dsin)
+    
+    for (var in vars){
+        if(  is.factor(dsin[[var]])){
+            dsin[[var]] <- as.character(dsin[[var]])
+        }
+    }
+    return(dsin)
+}
+
+
+
+
+#' has_unique_rows
+#' 
+#' Check if a data sets rows are unique
+#' @param DAT input data set (data frame)
+#' @param KEYS Set of keys which should be unique
+has_unique_rows <- function(DAT , KEYS){
+    DUPS <- duplicated( subset(DAT , select= KEYS) ) 
+    NDUPS <- sum( DUPS)
+    return( NDUPS == 0 )
+}
+
+#'convert_to_issue
+#'
+#'converts the count value into the correct issue format
+#'@param datin data inputted
+#'@importFrom tibble rownames_to_column
+convert_to_issue <- function(datin){
+    datin_tibble <- rownames_to_column(as.tibble(datin)) 
+    
+    names(datin_tibble) <- c('Variable', 'No of Differences')
+    
+    datin_tibble[ datin_tibble[["No of Differences"]] > 0, , drop = FALSE]
+}
+
---
+++ README.md
@@ -0,0 +1,36 @@
+# diffdf
+
+Find data.frame differences !
+
+## Introduction
+
+The diffdf package is designed to enable detailed comparison of two data.frames. Whilst many packages exist for informing you if there are differences between data.frames, none provide as much detail on what and where those differences are as diffdf does!
+
+## Example
+
+```
+library(diffdf)
+iris2 <- iris
+for ( i in 1:3) iris2[i,i] <- i^2
+iris2$new_var <- "hello"
+class(iris2$Species) <- "some class"
+dfdiff( iris, iris2)
+```
+
+## Features
+
+Currently diffdf supports the following:
+   - Checking for differences in values
+   - Checking for differences in attributes
+   - Checking for differences in classes
+   - Checking for differences in column names
+   - Checking for differences in the number of observations
+   - Matching rows by key/id variables
+   - Fuzzy comparisons (i.e. treating doubles and integers as the same)
+   - Extracting datasets of different rows
+   
+
+For more information on features please consult the vignette and man pages. 
+
+
+
