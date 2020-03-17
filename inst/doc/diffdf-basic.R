02d02358fb63059c465a270757586f0680c141a8
Craig Gower-Page <craig.gower-page@roche.com>
2020-03-17 22:10:03 +0000

    version 1.0.4

--- inst/doc/diffdf-basic.R
+++ inst/doc/diffdf-basic.R
@@ -1,10 +1,10 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(diffdf)
 
 LENGTH = 30
@@ -30,76 +30,67 @@ test_data
 
 diffdf( test_data , test_data)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data 
 test_data2 <- test_data2[,-6]
 diffdf(test_data , test_data2 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data
 test_data2 <- test_data2[1:(nrow(test_data2) - 2),]
 diffdf(test_data, test_data2 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data
 test_data2[5,2] <- 6
 diffdf(test_data , test_data2 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data
-test_data2[5,2] <- "blah"
+test_data2[,2] <- as.character(test_data2[,2])
 diffdf(test_data , test_data2 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data
 attr(test_data$ID , "label") <- "This is a interesting label"
 attr(test_data2$ID , "label") <- "what do I type here?"
 diffdf(test_data , test_data2 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data
 levels(test_data2$CATEGORICAL) <- c(1,2,3)
 diffdf(test_data , test_data2 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 test_data2 <- test_data
 test_data2$INTEGER[c(5,2,15)] <- 99L 
 diffdf( test_data , test_data2 , keys = c("GROUP1" , "GROUP2") , suppress_warnings = T)
 
-## ---- eval=FALSE---------------------------------------------------------
-#  result <- diffdf(
-#      test_data ,
-#      test_data2 ,
-#      keys = c("GROUP1" , "GROUP2") ,
-#      outfile =  "reports/diffdf.log"
-#  )
-#  
-
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 iris2 <- iris
 for (i in 1:3) iris2[i,i] <- 99
 diff <- diffdf( iris , iris2, suppress_warnings = TRUE)
 diffdf_issuerows( iris , diff)
 diffdf_issuerows( iris2 , diff)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 diffdf_issuerows( iris2 , diff , vars = "Sepal.Length")
 diffdf_issuerows( iris2 , diff , vars = c("Sepal.Length" , "Sepal.Width"))
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 iris2 <- iris
 for (i in 1:3) iris2[i,i] <- 99
 diff <- diffdf( iris , iris2, suppress_warnings = TRUE)
 
 diffdf_has_issues(diff)
 
-## ----eval = FALSE--------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  if ( diffdf_has_issues(diff)){
 #      #<Further programming steps / logic>
 #  }
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 dsin1 <- data.frame(x = 1.1e-06)
 dsin2 <- data.frame(x = 1.1e-07)
 
@@ -107,7 +98,7 @@ diffdf(dsin1  , dsin2 , suppress_warnings = T)
 
 diffdf(dsin1  , dsin2 , tolerance = 0.001 , suppress_warnings = T)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 dsin1 <- data.frame(x = as.integer(c(1,2,3)))
 dsin2 <- data.frame(x = as.numeric(c(1,2,3)))
 
